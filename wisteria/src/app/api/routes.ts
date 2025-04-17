c6f7057195d56ef7bbfa7a759faa3d364a707d52
kevskizhang <kevin.yiyu.zhang@gmail.com>
2025-04-16 22:27:45 -0500

    changed config from ts to js so that it is compatible on newer next versions

--- wisteria/src/app/api/routes.ts
+++ wisteria/src/app/api/routes.ts
@@ -2,21 +2,21 @@
 // import type { NextApiRequest, NextApiResponse } from "next";
 // import { getPool } from "../../../lib/db";
 
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   if (req.method === "GET") {
-//     try {
-//       const pool = getPool();
-//       const result = await pool.query("SELECT * FROM items");
-//       res.status(200).json(result.rows);
-//     } catch (error) {
-//       console.error("Database error:", error);
-//       res.status(500).json({ error: "Failed to fetch data" });
-//     }
-//   } else {
-//     res.setHeader("Allow", ["GET"]);
-//     res.status(405).end(`Method ${req.method} Not Allowed`);
-//   }
-// }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      const pool = getPool();
+      const result = await pool.query("SELECT * FROM userData");
+      res.status(200).json(result.rows);
+    } catch (error) {
+      console.error("Database error:", error);
+      res.status(500).json({ error: "Failed to fetch data" });
+    }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
